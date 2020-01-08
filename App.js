7765d1946b9efdc537320f9dc906ced3aeaa100d
Александра <alexa@MacBook-Air-Aleksandra.local>
2020-01-08 21:56:42 +0300

    init commit

---
+++ App.js
@@ -0,0 +1,76 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  FlatList,
+  Image
+} from 'react-native';
+
+import Header from 'components/Header';
+import RestaurantRow from 'components/RestaurantRow';
+
+import PizzaImage from 'images/pizza.png';
+
+const restaurants = [
+  {name: 'React cafe', address: '123 Anywhere St'},
+  {name: 'Fancy Cafe', address: '123 Main St'},
+  {name: 'Taco cafe', address: '123 Maple Rd'},
+]
+
+export default class RestaurantList extends React.Component {
+  state = {
+    search: null
+  }
+
+  render() {
+    return(
+      <View style={{
+        flex: 1
+      }}>
+        <View style={{
+          marginTop: 50,
+          alignItems: 'center'
+        }}>
+          <Image source={PizzaImage} />
+        </View>
+        <Header />
+
+        <TextInput 
+          style={ styles.input }
+          placeholder='Live search'
+          onChangeText={text => {
+            this.setState({ search: text })
+          }}
+          value={this.state.search}
+        />
+
+        <FlatList
+          data = { 
+            restaurants.filter(place => {
+              return !this.state.search ||
+              place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+            })
+          }
+          renderItem = {({ item, index }) => 
+            <RestaurantRow place={item} index={index} />
+          }
+          keyExtractor = {item => item.name}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 30,
+    padding: 10,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    borderColor: '#ddd',
+    backgroundColor: '#f5f5f5'
+  }
+});
---
+++ src/components/RestaurantRow.js
@@ -0,0 +1,108 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class RestaurantRow extends React.Component {
+  state = {
+    showInfo: false
+  }
+
+  infoPressed = () => {
+    this.setState({ showInfo: !this.state.showInfo });
+  }
+
+  render() {
+    const {
+      place,
+      index
+    } = this.props;
+
+    return(
+      <View key={place.name} style={{ backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }}>
+
+        <View style={styles.row}>
+          <View style={styles.stars}>
+            <Icon name="star" color='#ffd64c' />
+            <Icon name="star" color='#ffd64c' />
+            <Icon name="star-half" color='#ffd64c' />
+          </View>
+          <View style={styles.nameAddress}>
+            <Text>{place.name}</Text>
+            <Text style={styles.addressText}>{place.address}</Text>
+          </View>
+          <View style={styles.edges}>
+            <TouchableOpacity 
+              onPress={this.infoPressed}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Info</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {
+          this.state.showInfo &&
+          <View style={styles.info}>
+            <Text>Restaurant Info</Text>
+          </View>
+        }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row'
+  },
+  edges: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
+    minWidth: 50
+  },
+  stars: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    padding: 5,
+    minWidth: 50
+  },
+  nameAddress: {
+    flexDirection: 'column',
+    flex: 8
+  },
+  addressText: {
+    color: 'grey'
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#0066cc',
+    borderRadius: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    backgroundColor: '#fff'
+  },
+  buttonText: {
+    color: '#0066cc',
+    fontSize: 12
+  },
+  info: {
+    marginHorizontal: 40,
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4
+  }
+});
