4d71d52997f0a7811a4f9b09e7a25c7fb46e660b
Александра <alexa@MacBook-Air-Aleksandra.local>
2020-01-12 12:50:11 +0300

    Icon

--- src/components/Header.android.js
+++ src/components/Header.android.js
@@ -3,10 +3,10 @@ import { Text } from 'react-native';
 
 import HeaderStyle from '/styles/HeaderStyle';
 
-export default class Header extends Component {
-  render() {
-    return(
-      <Text style={HeaderStyle.header}>Restaurant Review</Text>
-    )
-  }
-}
+// export default class Header extends Component {
+//   render() {
+//     return(
+//       <Text style={HeaderStyle.header}>Restaurant Review</Text>
+//     )
+//   }
+// }
